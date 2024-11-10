a12472fb1d022a4d5b047bec6d12d468dd61bfd2
theSPGN <spgn1226@gmail.com>
2024-11-10 23:33:51 +0100

    specify used libraries

--- boost_tree_model.R
+++ boost_tree_model.R
@@ -1,5 +1,8 @@
-library(tidyverse)
-library(tidymodels)
-library(openair)
+# load used libraries
+library(tidymodels) # collection of packages for modeling
+library(dplyr) # data manipulation
+library(xgboost) # engine for boost_tree model
+library(yardstick) # metrics for model
+library(vip) # parameters importance
 
 load("prepared_data.RData")
