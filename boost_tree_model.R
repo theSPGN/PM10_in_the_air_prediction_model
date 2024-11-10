d95fa8b1155034e806e0bf4d7656333d2504743b
theSPGN <spgn1226@gmail.com>
2024-11-10 23:35:46 +0100

    model definition

--- boost_tree_model.R
+++ boost_tree_model.R
@@ -6,3 +6,15 @@ library(yardstick) # metrics for model
 library(vip) # parameters importance
 
 load("prepared_data.RData")
+# %%
+xgb_mod <-
+    boost_tree(
+        trees = 1000,
+        mtry = 8,
+        min_n = tune()
+    ) |>
+    set_engine(
+        engine = "xgboost",
+        num.threads = parallel:detectCores() - 1
+    ) |>
+    set_mode("regression")
