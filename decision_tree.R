9571b44bf6e789399a7847d92d46184472875eef
Jakub Wilk <kub.wilk@gmail.com>
2024-11-15 18:01:26 +0100

    Refined the model and successfully tested its functionality

--- decision_tree.R
+++ decision_tree.R
@@ -11,21 +11,23 @@ load("prepared_data.RData")
 #                          autoWidth = TRUE, 
 #                          scrollX = TRUE))
 
-recipe_tree <- recipe(grimm_pm10 ~ ., data = train_data)  |> 
+
+recipe_tree <- recipe(grimm_pm10 ~ ., data = train_data) |>
   step_rm(date) |> 
   step_date(all_nominal(), features = c("dow", "month")) |> 
   step_time(all_nominal(), features = c("hour")) |> 
-  step_corr(all_numeric(), threshold = 0.85) |> 
+  step_corr(all_numeric(), -all_outcomes(), threshold = 0.85) |> 
   step_normalize(all_numeric(), -all_outcomes()) |> 
   step_nzv(all_predictors()) |> 
-  step_pca(all_numeric(), threshold = 0.95) |> 
-  step_dummy(all_nominal()) 
+  step_pca(all_numeric(), -all_outcomes(), threshold = 0.95) |> 
+  step_dummy(all_nominal())
+
+
 
 # Sprawdzenie receptury
 prepped_recipe <- prep(recipe_tree, training = train_data)
 baked_data <- bake(prepped_recipe, new_data = NULL)
 
-head(baked_data)
 
 # Wyświetlenie przetworzonych danych
 datatable(baked_data, 
@@ -52,26 +54,26 @@ tree_grid <- grid_regular(
   cost_complexity(range = c(-3, -1)), 
   tree_depth(range = c(5, 15)),
   min_n(range = c(2, 10)),        
-  levels = 5
+  levels = 10
 )
 
 # Ustawienie walidacji krzyżowej dla trenowania
-set.seed(123)
-folds <- vfold_cv(train_data, v = 5, strata = grimm_pm10)
+set.seed(456)
+folds <- vfold_cv(train_data, v = 10, strata = grimm_pm10)
 
 # Strojenie parametrów przy użyciu siatki i walidacji krzyżowej
 tune_results <- tune_grid(
   workflow_tree,
   resamples = folds,
   grid = tree_grid,
-  metrics = metric_set(rmse, rsq)   
+  metrics = metric_set(rmse, rsq)
 )
 
 # Wyświetlenie najlepszych wyników
-show_best(tune_results, "rmse")
+show_best(tune_results, metric = "rmse")
 
 # Wybór najlepszej kombinacji parametrów na podstawie RMSE
-best_params <- select_best(tune_results, "rmse")
+best_params <- select_best(tune_results, metric = "rmse")
 
 # Finalizacja workflow z najlepszymi parametrami
 final_workflow <- finalize_workflow(workflow_tree, best_params)
@@ -84,3 +86,17 @@ final_results <- last_fit(final_workflow, split = data_split)
 
 # Wyświetlenie końcowej wydajności na zbiorze testowym
 collect_metrics(final_results)
+
+
+# dla level 5 oraz 5 folds
+# .metric .estimator .estimate .config             
+# <chr>   <chr>          <dbl> <chr>               
+#   1 rmse    standard      14.7   Preprocessor1_Model1
+# 2 rsq     standard       0.848 Preprocessor1_Model1
+
+# dla level 10 oraz 10 folds
+# .metric .estimator .estimate .config             
+# <chr>   <chr>          <dbl> <chr>               
+#   1 rmse    standard      14.7   Preprocessor1_Model1
+# 2 rsq     standard       0.847 Preprocessor1_Model1
+
