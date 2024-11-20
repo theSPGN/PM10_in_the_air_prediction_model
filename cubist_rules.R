97cd83112b54a1fdbfe4da720c11491adf1ff677
WildestPands <prus.maria.krk@gmail.com>
2024-11-20 21:22:38 +0100

    Workflows + grid
    1. creating workflows
    2. creating grid
    3. stuck on grid issue (out of index)

--- cubist_rules.R
+++ cubist_rules.R
@@ -3,6 +3,10 @@ load("prepared_data.RData")
 #working on train_data, test_data, val_set, other_station_data, data_split
 
 # creating cubist_rules model
+library(rules)
+library(Cubist)
+library(tidymodels)
+
 
 # cubist rules combines methods of decision trees and regression models
 cr_mod <- cubist_rules(
@@ -20,3 +24,28 @@ cr1_recipe <- recipe(grimm_pm10 ~., data = train_data,
                   update_role(date, new_role = "ID"))
 #2
 cr2_recipe <- recipe(grimm_pm10 ~n_0750+ws+pres, data = train_data)
+
+#workflows
+workflow_1 <- workflow() |>
+  add_recipe(cr1_recipe)|>
+  add_model(cr_mod)
+
+workflow_2 <- workflow() |>
+  add_recipe(cr2_recipe)|>
+  add_model(cr_mod)
+#building grid
+grid <- grid_regular(
+  committees(range = c(1, 100)),
+  neighbors(range = c(0,9)),
+  max_rules(range = c(1,10))
+)
+
+#tuning
+set.seed(666)
+cr1_res <- 
+  workflow_1 |> 
+  tune_grid(resamples = val_set, 
+            grid = grid,
+            control = control_grid(save_pred = T),
+            metrics = metric_set(rmse, rsq, mae))
+rlang::last_error()
